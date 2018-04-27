2678a33be3e3f233e60735e168f887146f358cdd
DougArrow <36687428+DougArrow@users.noreply.github.com>
2018-04-27 15:19:58 +0700

    typegen

---
+++ tg.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include <iostream>
+
+int srand(int(clock));
+
+//remake
+void gen(int t, int type, const int masSize, int *array)
+{
+	if (t == 0)
+	{
+		for (int i = 0; i < masSize; i++)
+		{
+			array[i] = rand();
+		}
+	}
+	else if (t == 1)//good
+	{
+		if (type == 1) //bubble
+		{
+			for (int i = 0; i < masSize; i++)
+			{
+				array[i] = i;
+			}
+		}
+		else if (type == 2) //comb
+		{
+			for (int i = 0; i < masSize; i++)
+			{
+				array[i] = i;
+			}
+		}
+		else //radix
+		{
+			for (int i = 0; i < masSize; i++)
+			{
+				array[i] = i;
+			}
+		}
+	}
+	else//bad
+	{
+		if (type == 1) //bubble
+		{
+			for (int i = 0; i < masSize; i++)
+			{
+				array[i] = masSize - i;
+			}
+		}
+		else if (type == 2) //comb
+		{
+			for (int i = 0; i < masSize; i++)
+			{
+				array[i] = masSize - i;
+			}
+		}
+		else //radix
+		{
+			for (int i = 0; i < masSize; i++)
+			{
+				array[i] = masSize - i;
+			}
+		}
+	}
+}
